8acfe26d4bfe50ae0ad9f7239abb2e15be9b6779
jorislimonier <joris.limonier@hotmail.fr>
2022-02-06 19:20:02 +0100

    comment data assembler script and rename docs/->references/

--- src/data-assembler.py
+++ src/data-assembler.py
@@ -1,9 +1,5 @@
-# %%
 import glob
 import pandas as pd
-from pyparsing import col
-
-# %%
 
 
 class DataAssembler:
@@ -14,6 +10,9 @@ class DataAssembler:
     FILE_ENDINGS = {DOC_ENDING, TAB_ENDING}
 
     def split_file_names(self):
+        """
+        Split file names into a radical and an extension
+        """
         raw_files = glob.glob(f"{self.RAW_PATH}*")
         raw_files = [file.removeprefix(self.RAW_PATH) for file in raw_files]
         rad_ext = {}
@@ -21,6 +20,7 @@ class DataAssembler:
             radical = "_".join(file.split("_")[:-1])  # remove everything after last "_"
             ext = file.removeprefix(f"{radical}_")
 
+            # Create dict entry if key not in dict keys, else append to existing value
             if radical in rad_ext.keys():
                 rad_ext[radical] += [ext]
             else:
@@ -29,6 +29,9 @@ class DataAssembler:
 
     @property
     def radicals_to_assemble(self):
+        """
+        Keep radicals if there exists a file ending in DOC_ENDING and one ending in TAB_ENDING
+        """
         rad_ext = self.split_file_names()
         radicals_to_assemble = []
         for radical, suf in rad_ext.items():
@@ -37,6 +40,9 @@ class DataAssembler:
         return radicals_to_assemble
 
     def assemble(self):
+        """
+        Puts columns and data together
+        """
         for radical in self.radicals_to_assemble:
             doc_file = "_".join([radical, self.DOC_ENDING])
             csv_file = "_".join([radical, self.TAB_ENDING])
@@ -59,5 +65,3 @@ class DataAssembler:
 
 assembler = DataAssembler()
 assembler.assemble()
-
-# %%
