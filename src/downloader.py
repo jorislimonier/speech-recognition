e876b38838458718884096e5d39d08bb523fc945
jorislimonier <joris.limonier@hotmail.fr>
2022-02-06 15:29:14 +0100

    complete downloader

--- src/downloader.py
+++ src/downloader.py
@@ -5,13 +5,21 @@ import requests
 from bs4 import BeautifulSoup
 
 # %%
-BASE_URL = "https://catalog.ldc.upenn.edu/docs/LDC97S62/"
-req = requests.get(url=BASE_URL)
-soup = BeautifulSoup(req.content, "html.parser")
+class Downloader:
+    def __init__(self) -> None:
+        pass
 
-files = [tr.td.a.get("href") for tr in soup.tbody.find_all("tr")][1:]
-files
-# %%
-# %%
-response = urllib.request.urlretrieve(BASE_URL + files[1], f"data/{files[1]}")
-print(response)
+    BASE_URL = "https://catalog.ldc.upenn.edu/docs/LDC97S62/"
+
+    def download(self, filename=None):
+        req = requests.get(url=self.BASE_URL)
+        soup = BeautifulSoup(req.content, "html.parser")
+
+        files = [tr.td.a.get("href") for tr in soup.tbody.find_all("tr")][1:]
+        for file in files:
+            response = urllib.request.urlretrieve(
+                url=self.BASE_URL + file,
+                filename=f"../data/raw/{file}",
+            )
+downloader = Downloader()
+downloader.download()
