2226eafa21b16e6a82ce92a0951b5a1f2f8c2d50
jorislimonier <joris.limonier@hotmail.fr>
2022-03-13 16:11:53 +0100

    make class for transcription

--- src/transcribe.py
+++ src/transcribe.py
@@ -7,62 +7,59 @@ import librosa
 import soundfile as sf
 
 
-model_file_path = "models/deepspeech-0.9.3-models.pbmm"
-beam_width = 500
-model = deepspeech.Model(model_file_path)
+class Transcribe:
+    def __init__(
+        self,
+        model_filepath="models/deepspeech-0.9.3-models.pbmm",
+    ):
+        self.model_filepath = model_filepath
+        self.model = deepspeech.Model(self.model_filepath)
 
-filename = "data/raw/train-ws96-i/wav/20/2073B/sw2073B-ws96-i-0025.wav"
+    def rewrite_wav(self, filepath):
+        x, _ = librosa.load(filepath, sr=16000)
+        tmp = "data/interim/tmp.wav"
+        sf.write(tmp, x, 16000)
+        return tmp
+
+    def batch_transcribe(self, filepath):
+        # batch api
+        tmp = self.rewrite_wav(filepath)
+
+        w = wave.open(tmp, "r")
+        frames = w.getnframes()
+        buffer = w.readframes(frames)
+        data16 = np.frombuffer(buffer, dtype=np.int16)
+
+        text = self.model.stt(data16)
+        print(text)
+
+    def stream_transcribe(self, filepath):
+        # stream api
+        tmp = self.rewrite_wav(filepath)
+        w = wave.open(tmp, "r")
+        frames = w.getnframes()
+        buffer = w.readframes(frames)
+
+        context = self.model.createStream()
+
+        buffer_len = len(buffer)
+        offset = 0
+        batch_size = 16384
+        text = ""
+        while offset < buffer_len:
+            end_offset = offset + batch_size
+            chunk = buffer[offset:end_offset]
+            data16 = np.frombuffer(chunk, dtype=np.int16)
+            context.feedAudioContent(data16)
+            text = context.intermediateDecode()
+            print(text)
+            offset = end_offset
+
+
+trsc = Transcribe()
+filepath = "data/raw/train-ws96-i/wav/20/2073B/sw2073B-ws96-i-0025.wav"
 # filename = "data/external/audio/2830-3980-0043.wav"
 
 
-def rewrite_wav():
-    x, _ = librosa.load(filename, sr=16000)
-    tmp = "data/interim/tmp.wav"
-    sf.write(tmp, x, 16000)
-    return tmp
-
-
-def batch_transcribe(filepath):
-    # batch api
-    tmp = rewrite_wav()
-
-    w = wave.open(tmp, "r")
-    rate = w.getframerate()
-    frames = w.getnframes()
-    buffer = w.readframes(frames)
-    # print(rate)
-    # print(model.sampleRate())
-
-    data16 = np.frombuffer(buffer, dtype=np.int16)
-
-    text = model.stt(data16)
-    print(text)
-
-
-batch_transcribe(filename)
-
-
-def stream_transcribe():
-    # stream api
-    tmp = rewrite_wav()
-    w = wave.open(tmp, "r")
-    frames = w.getnframes()
-    buffer = w.readframes(frames)
-
-    context = model.createStream()
-
-    buffer_len = len(buffer)
-    offset = 0
-    batch_size = 16384
-    text = ""
-    while offset < buffer_len:
-        end_offset = offset + batch_size
-        chunk = buffer[offset:end_offset]
-        data16 = np.frombuffer(chunk, dtype=np.int16)
-        context.feedAudioContent(data16)
-        text = context.intermediateDecode()
-        print(text)
-        offset = end_offset
-
-
-stream_transcribe()
+trsc.batch_transcribe(filepath)
+# trsc.stream_transcribe(filepath)
