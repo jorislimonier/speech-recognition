a46b1b7f79033c9eb25fef97a0245ceb0d7332f9
jorislimonier <joris.limonier@hotmail.fr>
2022-03-19 12:11:23 +0100

    plot WER by removing worst samples

--- src/analysis.py
+++ src/analysis.py
@@ -1,6 +1,8 @@
+from itertools import chain
 import re
 
 import jiwer
+from jiwer.measures import _preprocess
 import pandas as pd
 
 
@@ -12,6 +14,12 @@ class Analysis:
     def clean_ground_truth(txt):
         """apply successive cleanings to ground truth"""
         txt = txt.replace("H#", "")
+        txt = txt.replace("h#", "")
+        txt = txt.replace("_#", "")
+        txt = txt.replace("_!", "")
+        txt = txt.replace("_?", "")
+        txt = txt.replace('"', "")
+        txt = txt.replace("_", " ")
         txt = re.sub("\[.*?\]", "", txt)  # remove words between square brackets
         txt = re.sub("  ", " ", txt)  # make multiple whitespaces to single
         txt = txt.strip()
@@ -25,31 +33,44 @@ class Analysis:
         """
         df = self.df_res.copy()
 
-        # clean
-        df["ground_truth"] = df["ground_truth"].apply(self.clean_ground_truth)
-
         # transformations for metrics computation
         transformation = jiwer.Compose(
             [
                 jiwer.ToUpperCase(),
                 jiwer.RemoveWhiteSpace(replace_by_space=True),
                 jiwer.RemoveMultipleSpaces(),
+                jiwer.RemovePunctuation(),
+                jiwer.RemoveKaldiNonWords(),
+                jiwer.ExpandCommonEnglishContractions(),
                 jiwer.ReduceToListOfListOfWords(word_delimiter=" "),
             ]
         )
 
+        # clean
+        df["clean_ground_truth"] = df["ground_truth"].apply(self.clean_ground_truth)
+
         # iterate over rows and set value for each metric
         for row_nb in range(len(df)):
-            ground_truth = df.loc[row_nb, "ground_truth"]
+            clean_ground_truth = df.loc[row_nb, "clean_ground_truth"]
             pred = df.loc[row_nb, "pred"]
-
+            
             # skip if isna
             if pd.isna(pred):
                 continue
+            
+            # preproc = _preprocess(
+            #     truth=clean_ground_truth,
+            #     hypothesis=pred,
+            #     truth_transform=transformation,
+            #     hypothesis_transform=transformation,
+            # )
+            # print(preproc)
+            # df.loc[row_nb, "clean_ground_truth"] = preproc[0]
+
 
             # compute dictionary with all metrics
             measures = jiwer.compute_measures(
-                truth=ground_truth,
+                truth=clean_ground_truth,
                 hypothesis=pred,
                 truth_transform=transformation,
                 hypothesis_transform=transformation,
@@ -59,7 +80,7 @@ class Analysis:
             for measure_name, measure_val in measures.items():
                 df.loc[row_nb, measure_name] = measure_val
 
-        return df[list(measures.keys())]
+        return df[["clean_ground_truth"] + list(measures.keys())]
 
     def count_keywords(self):
         """
